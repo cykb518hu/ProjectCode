c43bc6cf75f29e40784abbb2c14a851d5e6d8d2c
cykb518hu <463372303@qq.com>
2017-04-04 19:47:32 +0800

    fisrt version
    checkin

---
+++ SingleApplication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessHandler.MessageHandler;
+using BusinessHandler.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingleApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DataDetail()
+        {
+            return View();
+        }
+
+        public JsonResult GetDataList(DocQueryMessage message)
+        {
+            var docQuery = new DocQueryFactory();
+            var result = docQuery.GetDocQueryResult(message);
+            var total = result.Count;
+            var rows = result.Skip(message.offset).Take(message.limit).ToList();
+            return Json(new { total = total, rows = rows }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult SaveComment(DocQueryResultModel message)
+        {
+            var docQuery = new DocQueryFactory();
+            docQuery.UpdateQuery(message);
+            return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult LoadCityList()
+        {
+            var docQuery = new DocQueryFactory();
+            var result = docQuery.GetCityList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ SingleApplication/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿
+using Autofac;
+using Autofac.Integration.Mvc;
+using BusinessHandler.MessageHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SingleApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutofacConfig.ConfigureContainer();
+        }
+        public class AutofacConfig
+        {
+            public static void ConfigureContainer()
+            {
+                var builder = new ContainerBuilder();
+                builder.RegisterControllers(typeof(MvcApplication).Assembly);
+                builder.RegisterType<DocQueryCSVRepository>().As<IDocQueryRepository>();
+                builder.RegisterType<AspNetCacheRepository>().As<ICacheRepository>();
+                builder.RegisterFilterProvider();
+
+                var container = builder.Build();
+                DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            }
+        }
+    }
+}
