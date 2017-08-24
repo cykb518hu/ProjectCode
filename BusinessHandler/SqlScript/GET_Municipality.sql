50fdee5f1ca2ca71fb06a89d5caebe17c425f2ad
Achilles.Hu <Achilles.Hu@CHELPR-NW10002>
2017-08-24 18:32:16 +0800

    new procedure

---
+++ BusinessHandler/SqlScript/GET_Municipality.sql
@@ -0,0 +1,66 @@
+IF EXISTS (SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID(N'[dbo].[GET_Municipality]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[GET_Municipality]
+GO
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[GET_Municipality]
+(
+@OrderByField nvarchar(100)=null,
+@CityName varchar(max) =null,
+@CountyName varchar(max) =null,
+@KeyWord varchar(max) =null,
+@MeetingDate varchar(50)=null,
+@DeployeDate varchar(50)=null,
+@IsChecked varchar(5)=null,
+@IsImportant varchar(5)=null
+)
+as
+declare @sqlstr varchar(max)
+begin
+
+set @sqlstr='
+SELECT C.LONG_NM, count(*) Number
+ FROM  DBO.DOCUMENT D INNER JOIN DBO.QUERY Q ON D.DOC_GUID=Q.DOC_GUID INNER JOIN DBO.QUERY_ENTRY QE ON QE.QUERY_GUID=Q.QUERY_GUID 
+ INNER JOIN DBO.CITY C ON C.CITY_NM=D.CITY_NM where 1=1'
+
+if @CityName is not null 
+	begin
+		set @sqlstr=@sqlstr+' and D.CITY_NM IN ('+ @CityName+')'
+	end
+if @CountyName is not null 
+	begin
+		set @sqlstr=@sqlstr+' and C.COUNTY_NM IN ('+ @CountyName+')'
+	end
+if @KeyWord is not null 
+	begin
+		set @sqlstr=@sqlstr+' and QE.KEYWORD IN ('+ @KeyWord+')'
+	end
+if @MeetingDate is not null 
+	begin
+		set @sqlstr=@sqlstr+' and Q.MEETING_DATE > '''+ @MeetingDate+''''
+	end
+if @DeployeDate is not null 
+	begin
+		set @sqlstr=@sqlstr+' and C.DEPLOYE_DATE = '''+ @DeployeDate+''''
+	end
+if @IsChecked is not null
+   begin
+		set @sqlstr=@sqlstr+' and D.CHECKED = '''+ @IsChecked+''''
+	end
+if @IsImportant is not null
+   begin
+		set @sqlstr=@sqlstr+' and D.IMPORTANT = '''+ @IsImportant+''''
+	end
+
+begin
+set @sqlstr=@sqlstr+' group by C.LONG_NM'
+exec(@sqlstr)
+end
+
+end
+
+GO
