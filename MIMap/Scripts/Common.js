4cdf16332eb7353f2e63cae751b9947194b016bf
cykb518hu <463372303@qq.com>
2017-09-24 21:16:51 +0800

    combie css

---
+++ MIMap/Scripts/Common.js
@@ -0,0 +1,18 @@
+﻿
+
+    function successTips() {
+        toastr.options = {
+            "positionClass": "toast-top-center",
+            "hideDuration": "300",//消失的动画时间
+            "timeOut": "2000",//展现时间
+        }
+        toastr.success('Success')
+    }
+    function errorTips(str) {
+        toastr.options = {
+            "positionClass": "toast-top-center",
+            "hideDuration": "300",//消失的动画时间
+            "timeOut": "2000",//展现时间
+        }
+        toastr.error(str);
+    }
